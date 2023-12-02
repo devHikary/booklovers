2dde5f0f9022141801f7cd715b1887fb7c0f62ea
devHikary <wrkkarina@gmail.com>
2023-12-02 15:31:22 -0300

    cadastro parcial livro

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,25 +1,44 @@
+import { Book } from 'src/app/models/book';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+const { urlAPI } = environment;
 
 @Injectable({
   providedIn: 'root'
 })
 export class BooksService {
-  private urlAPI = "https://www.googleapis.com/books";
-  private controllerName = "/v1/volumes?q=";
+  private urlAPIg = "https://www.googleapis.com/books";
+  private verApig = "/v1/volumes?q=";
+  private controllerName = "books/";
   constructor(
     private http: HttpClient,
   ) { }
 
+  getAllBooks(){
+    return this.http.get(`${urlAPI}${this.controllerName}`);
+  }
+
   getAllByTitle(title: string) {
-    return this.http.get(`${this.urlAPI}${this.controllerName}intitle:${title}`);
+    return this.http.get(`${this.urlAPIg}${this.verApig}intitle:${title}`);
   }
 
   getAllByCategories(categories: string) {
-    return this.http.get(`${this.urlAPI}${this.controllerName}subject:${categories}`);
+    return this.http.get(`${this.urlAPIg}${this.verApig}subject:${categories}`);
   }
 
   getById(id: string) {
-    return this.http.get(`${id}`);
+    // return this.http.get(`${id}`);
+    return this.http.get(`${urlAPI}${this.controllerName}${id}`);
+  }
+
+  public add(b: Book) {
+    let request = `${urlAPI}${this.controllerName}`;
+    return this.http.post(request, b);
+  }
+
+  public update(b: Book) {
+    let request = `${urlAPI}${this.controllerName}`;
+    return this.http.put(request, b);
   }
 }
